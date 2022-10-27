e44ba7f1493aa3a56e2f31ada24c4ffa71ef9bde
HuynhMinhNghia-B2012232 <nghiab2012232@student.ctu.edu.vn>
2022-10-27 15:03:25 +0700

    Tao trang cap nhat lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="contact" class="page">
+    <h4>Hiệu chỉnh Liên hệ</h4>
+    <ContactForm
+      :contact="contact"
+      @submit:contact="updateContact"
+      @delete:contact="deleteContact"
+    />
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+// import ContactService from "@/services/contact.service";
+export default {
+  components: {
+    ContactForm,
+  },
+};
+</script>
